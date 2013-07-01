65cef636460d7e10822e4541b83bdada864d934a
Florent Benoit <Florent.Benoit@peergreen.com>
2013-07-01 15:29:26 +0200

    Switch to the new metadata adapter

--- src/main/java/com/peergreen/webcontainer/WebApplication.java
+++ src/main/java/com/peergreen/webcontainer/WebApplication.java
@@ -1,11 +1,5 @@
 /**
- * Copyright 2013 Peergreen S.A.S.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2013 Peergreen S.A.S. All rights reserved.
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,7 +18,7 @@ import javax.naming.Context;
 
 import com.peergreen.deployment.Artifact;
 import com.peergreen.deployment.facet.archive.Archive;
-import com.peergreen.injection.AnnotatedClass;
+import com.peergreen.metadata.adapter.AnnotatedClass;
 
 public interface WebApplication {
 
